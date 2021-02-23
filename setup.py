7842fb08a1bb8277c860f60c12ae13f9851d00c3
Gabriel Martinez Iwasawa <68246311+Giwasawa@users.noreply.github.com>
2021-02-23 15:08:20 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name     = 'dcdevaluation',
-      version  = '0.7.10'              ,
+      version  = '0.8.0'              ,
       packages = ['dcdevaluation']     ,
       zip_safe = False              )
