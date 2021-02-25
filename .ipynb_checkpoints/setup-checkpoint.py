42ba18e8e84ea8434f748818c33ad25678d0cd0c
Gabriel Martinez Iwasawa <68246311+Giwasawa@users.noreply.github.com>
2021-02-25 16:49:10 -0300

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name     = 'dcdevaluation',
-      version  = '0.7.10'              ,
-      packages = ['dcdevaluation']     ,
-      zip_safe = False              )
